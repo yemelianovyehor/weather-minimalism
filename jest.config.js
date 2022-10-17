4204f9722d8810a072c4a399ef9dfb5563a4d826
Yehor Yemelianov <yemelianovehor@gmail.com>
2022-10-17 10:25:30 +0200

    Now it should work

--- jest.config.js
+++ jest.config.js
@@ -135,7 +135,7 @@ export default {
 	// setupFiles: [],
 
 	// A list of paths to modules that run some code to configure or set up the testing framework before each test
-	// setupFilesAfterEnv: [],
+	setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
 
 	// The number of seconds after which a test is considered as slow and reported as such in the results.
 	// slowTestThreshold: 5,
@@ -144,7 +144,7 @@ export default {
 	// snapshotSerializers: [],
 
 	// The test environment that will be used for testing
-	testEnvironment: "node",
+	testEnvironment: "jsdom",
 
 	// Options that will be passed to the testEnvironment
 	// testEnvironmentOptions: {},
@@ -164,7 +164,7 @@ export default {
 	// ],
 
 	// The regexp pattern or array of patterns that Jest uses to detect test files
-	// testRegex: [],
+	testRegex: ["((\\.|/*.)(test))\\.(js|jsx|ts|tsx)?$"],
 
 	// This option allows the use of a custom results processor
 	// testResultsProcessor: undefined,
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import outputWebVitals from "./WebVitals";
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>
 );
-
-outputWebVitals(console.log);
